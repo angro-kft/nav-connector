0f975f3978956916c7bfc9cf745119d1636e2497
Dávid Házi <kailniris92@gmail.com>
2018-03-09 12:48:07 +0100

    add env var parse

--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
+const { chain, camelCase } = require('lodash');
 require('dotenv').config();
 
 const NavConnector = require('../src/index.js');
 
+const technicalUser = chain(process.env)
+  .pickBy((value, key) => key.startsWith('NAV_CONNECTOR_'))
+  .mapKeys((value, key) => key.replace('NAV_CONNECTOR_', ''))
+  .mapKeys((value, key) => camelCase(key))
+  .value();
+
 describe('NavConnector', () => {
   it('should inherit from event emitter', done => {
-    const navConnector = new NavConnector();
+    const navConnector = new NavConnector(technicalUser);
 
     navConnector.on('foo', done);
 
