0484f7f1216894b3fce7bd3722c71b1c428458f8
Dávid Házi <kailniris92@gmail.com>
2018-03-14 08:56:35 +0100

    add create request signature

--- src/create-request-signature.js
+++ src/create-request-signature.js
@@ -1,17 +1,22 @@
-const ObjectId = require('bson-objectid');
 const { crc32 } = require('crc');
 const crypto = require('crypto');
 
+/**
+ * Create a request signature for a base request.
+ * @param {Object} params Function params.
+ * @param {string} params.requestId Unique string.
+ * @param {Date} params.date Request creation date.
+ * @param {String} params.signatureKey Technical user's signature key.
+ * @param {Array}[params.baseURL=[]] Invoice xmls in base64 encoding.
+ * @returns {string} Request signature.
+ */
 module.exports = function createRequestSignature({
-  requestId = ObjectId().toString(),
-  date = new Date(),
+  requestId,
+  date,
   signatureKey,
   invoices = [],
 }) {
-  const timestamp = `${date.toISOString().split('.')[0]}`
-    .replace(/T/g, '')
-    .replace(/-/g, '')
-    .replace(/:/g, '');
+  const timestamp = `${date.toISOString().split('.')[0]}`.replace(/[-:T]/g, '');
   const invoiceCrcChecksums = invoices.map(invoice => crc32(invoice)).join('');
 
   const hash = crypto.createHash('sha512');
--- test/create-request-signature.js
+++ test/create-request-signature.js
@@ -1,21 +1,47 @@
 const { assert } = require('chai');
+const ObjectId = require('bson-objectid');
 
 const createRequestSignature = require('../src/create-request-signature.js');
+
+const invoicesBase64 = require('./invoices-base64.js');
 
 describe('createRequestSignature()', () => {
   it('should be a function', () => {
     assert.isFunction(createRequestSignature);
   });
 
+  it('should return string', () => {
+    const requestSignature = createRequestSignature({
+      requestId: ObjectId,
+      date: new Date(),
+      signatureKey: 'mnlFCfiGkwjIDe59',
+      invoices: invoicesBase64,
+    });
+
+    assert.isString(requestSignature);
+  });
+
   it('should return valid request signature', () => {
     const requestSignature = createRequestSignature({
-      requestId: 'TSTKFT1222564',
-      date: new Date('2017-12-30T18:25:45Z'),
-      signatureKey: 'XqywhcD1o4JZXsSL',
-      invoices: [],
+      requestId: 'RID332893718672',
+      date: new Date('2018-01-22T20:45:58Z'),
+      signatureKey: 'mnlFCfiGkwjIDe59',
+      invoices: invoicesBase64,
     });
     const expected =
-      '0726C94424174913464010FAEB7B33E070EE619E6A3A869EB8A66FFCA26AE58002E4CC6CBF8A683E636441D11B3ACA4E04969FFEAAB9971A63C191918709EE52';
+      '01C9CAB158C61784CECACE792F67C68C94135654F10F0A36FCA50E3F18D3068E1B14AADBBCBF3BA08446FFAC037D1DC2D2F92D932FA5085C5B69C3615EB08D50';
+
+    assert.equal(requestSignature, expected);
+  });
+
+  it('should return valid request signature without invoices', () => {
+    const requestSignature = createRequestSignature({
+      requestId: 'RID253015243342',
+      date: new Date('2018-01-19T15:12:55Z'),
+      signatureKey: 'mnlFCfiGkwjIDe59',
+    });
+    const expected =
+      '9CF7E97AFE159C71A632D3D6C9F75C8B7012BFE23AAAC425C276DE997B31878FD9394426CCFE99983EDA55B244921399C2B8BB262ABED59DF9AFE3CDD76ED86F';
 
     assert.equal(requestSignature, expected);
   });
